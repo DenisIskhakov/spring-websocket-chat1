3973393a310f2c497c37bd9554b74dd63bb75bc4
DenisIskhakov <iskhakov.denis@mail.ru>
2023-05-03 21:18:11 +0300

    chat

--- frontend/js/chat.js
+++ frontend/js/chat.js
@@ -20,7 +20,7 @@ function connectToChat(userName) {
         });
     });
 }
-
+//.
 function sendMsg(from, text) {
     stompClient.send("/app/chat/" + selectedUser, {}, JSON.stringify({
         fromLogin: from,
